d6a7b6e4da810e96b1f444e323a48d62a5902b49
Willian Dias <willian.dias360@hotmail.com>
2020-06-14 19:57:36 -0300

    Primeiro commit

---
+++ app/src/main/java/br/com/willian/nybooks/presentation/books/BooksActivity.kt
@@ -0,0 +1,12 @@
+package br.com.willian.nybooks.presentation.books
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import br.com.willian.nybooks.R
+
+class BooksActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_books)
+    }
+}
